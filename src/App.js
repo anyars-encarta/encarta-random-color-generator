81a80a10122a4c4e78931426a082d7de471919a3
anyars-encarta <anyarsencarta@gmail.com>
2024-02-18 08:46:36 +0000

    Correct eslint errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import RandomColor from './components/random-color';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <RandomColor />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <RandomColor />
+  </div>
+);
 
 export default App;
